24e79ca3c1d58b3fb4342acd79900bb1c9bb158e
Alcboss <138582902+Alcboss@users.noreply.github.com>
2024-12-30 19:12:50 +0300

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm fucking alive!"
+    return "I'm alive!"  # Updated to be more professional
 
 def run():
     app.run(host='0.0.0.0', port=8080)
@@ -13,4 +13,3 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
-    
